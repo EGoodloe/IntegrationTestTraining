120e605e81e1b379a705434c920387c8e68dd186
taeatonDev <taeaton@gmail.com>
2015-12-02 14:44:02 -0600

    Draft Integration Test Workshop

---
+++ UnitTestWorkshop/UnitTestWorkshop.Business.IntegrationTests/Providers/UserAccountProvider_tests.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SMG.Core;
+using SMG.Core.Contracts;
+using SMG.Core.Data.Contracts;
+using UnitTestWorkshop.Business.Models.AccountModels;
+using UnitTestWorkshop.Business.Providers;
+using UnitTestWorkshop.Business.Translators;
+using UnitTestWorkshop.Data.Models.AccountModels;
+using UnitTestWorkshop.Data.Models.QueryModels;
+
+namespace UnitTestWorkshop.Business.IntegrationTests.Providers
+{
+    [TestClass]
+    public class UserAccountProvider_tests
+    {
+        private static Mock<ICreatable<User>> _mockCreateableUser;
+        private static Mock<IUpdatable<User>> _mockUpdateableUser;
+        private static Mock<IRetrievable<ByUserId, User>> _mockRetrievableUserById;
+        private static Mock<IDeletable<User>> _mockDeletableUser;
+        private static Mock<ISystemTime> _mockSystemTime;
+        private static Mock<IBulkRetrievable<ByEncodedUserId, UserAuthentication>> _mockRetrievableAuthyId;
+        private static Mock<IUpdatable<UserAuthentication>> _mockUpdatableAuth;
+        private static Mock<ICreatable<UserAuthentication>> _mockCreatableAuth;
+        private static Mock<IDeletable<UserAuthentication>> _mockDeletableAuth;
+        private static Mock<IRetrievable<ByUserEmail, User>> _mockRetrievableUserByEmail;
+        private IPasswords _passwords;
+        private ITranslate<User, UserAccount> _translateUserUserAuth;
+        private IUserAuthenticationProvider _userAuthProvider;
+        private UserAccountProvider _subject;
+
+        [ClassInitialize]
+        [Description("Initializes only once per full test run.")]
+        public static void InitialSetup(TestContext context)
+        {
+            //needed for account provider
+            _mockCreateableUser = new Mock<ICreatable<User>>();
+            _mockUpdateableUser = new Mock<IUpdatable<User>>();
+            _mockRetrievableUserById = new Mock<IRetrievable<ByUserId, User>>();
+            _mockDeletableUser = new Mock<IDeletable<User>>();
+            _mockSystemTime = new Mock<ISystemTime>();
+
+            //needed for auth provider
+            _mockRetrievableAuthyId = new Mock<IBulkRetrievable<ByEncodedUserId, UserAuthentication>>();
+            _mockUpdatableAuth = new Mock<IUpdatable<UserAuthentication>>();
+            _mockCreatableAuth = new Mock<ICreatable<UserAuthentication>>();
+            _mockDeletableAuth = new Mock<IDeletable<UserAuthentication>>();
+
+            //needed for both account provider and auth provider
+            _mockRetrievableUserByEmail = new Mock<IRetrievable<ByUserEmail, User>>();
+        }
+
+        [TestInitialize]
+        [Description("Initializes before every Test.")]
+        public void InitializeProvider()
+        {
+            _passwords = new Passwords();
+            _translateUserUserAuth = new DataUserToAccountUserTranslator();
+
+            _userAuthProvider = new UserAuthenticationProvider(
+                _translateUserUserAuth
+                , _mockRetrievableUserByEmail.Object
+                , _mockRetrievableAuthyId.Object
+                , _mockUpdatableAuth.Object
+                , _mockCreatableAuth.Object
+                , _passwords
+                , _mockDeletableAuth.Object);
+
+            _subject = new UserAccountProvider(
+                _userAuthProvider
+                , _mockCreateableUser.Object
+                , _mockRetrievableUserById.Object
+                , _mockRetrievableUserByEmail.Object
+                , _mockDeletableUser.Object
+                , _mockUpdateableUser.Object
+                , _translateUserUserAuth
+                , _mockSystemTime.Object);
+        }
+
+        [TestMethod]
+        public void Retrieve_Account_With_Valid_Credentials()
+        {
+            //Data Layer Arrange
+            var dataUser = new User
+            {
+                CreationDate = new DateTime(2000,01,20),
+                Email = "HandyManJack@AOL.com",
+                FirstName = "Jack",
+                LastName = "Hoffman",
+                UserId = "551581bf90eb291bd0e97fb2",
+                LastLogin = new DateTime(2012,10,31)
+            };
+
+            _mockRetrievableUserById
+                .Setup(x => x.Retrieve(It.IsAny<ByUserId>()))
+                .Returns(dataUser);
+
+            _mockRetrievableUserByEmail
+                .Setup(x => x.Retrieve(It.IsAny<ByUserEmail>()))
+                .Returns(dataUser);
+
+            _mockRetrievableAuthyId
+                .Setup(x => x.RetrieveAll(It.IsAny<ByEncodedUserId>()))
+                .Returns(new List<UserAuthentication>
+                {
+                    new UserAuthentication
+                    {
+                        AccountActive = true,
+                        AccountType = AccountType.ActualAccount,
+                        EncodedPassword = "N6Yblj8+jm04p7AoXji9jxKSq21tsalRS/UBTa8fiV4=",
+                        EncodedUserId = "Yvf08ew7hUptyomgslYIYhQmJ+jhUM/nrgQtbJgnW50=",
+                        EncryptionKey = "ApplePear",
+                        FailedLoginAttemptCount = 0
+                    },
+                    new UserAuthentication
+                    {
+                        AccountActive = true,
+                        AccountType = AccountType.Trap1Account,
+                        EncodedPassword = "Blue",
+                        EncodedUserId = "Yvf08ew7hUptyomgslYIYhQmJ+jhUM/nrgQtbJgnW50=",
+                        EncryptionKey = "PearBanana",
+                        FailedLoginAttemptCount = 0
+                    },
+                    new UserAuthentication
+                    {
+                        AccountActive = true,
+                        AccountType = AccountType.Trap2Account,
+                        EncodedPassword = "Greed",
+                        EncodedUserId = "Yvf08ew7hUptyomgslYIYhQmJ+jhUM/nrgQtbJgnW50=",
+                        EncryptionKey = "BananaGrape",
+                        FailedLoginAttemptCount = 0
+                    },
+                    new UserAuthentication
+                    {
+                        AccountActive = true,
+                        AccountType = AccountType.Trap3Account,
+                        EncodedPassword = "Yellow",
+                        EncodedUserId = "Yvf08ew7hUptyomgslYIYhQmJ+jhUM/nrgQtbJgnW50=",
+                        EncryptionKey = "GrapeApple",
+                        FailedLoginAttemptCount = 0
+                    }
+                });
+
+            //arrange
+            _mockSystemTime
+                .Setup(x => x.Current())
+                .Returns(new DateTime(2015, 12, 2, 0, 0, 0, DateTimeKind.Utc));
+
+            var input = new Credentials
+            {
+                Email = "HandyManJack@AOL.com",
+                Password = "SnakesAreSlippery"
+            };
+
+            var expected = new UserAccount
+            {
+                Email = "HandyManJack@AOL.com",
+                FirstName = "Jack",
+                LastName = "Hoffman",
+                UserId = "551581bf90eb291bd0e97fb2"
+            };
+
+            UserAccount actual;
+
+            //act
+            actual = _subject.RetrieveAccount(input);
+
+            //assert
+            Assert.AreEqual(expected.Email, actual.Email, string.Format("Expected Email of {0}, actual was {1}", expected.Email, actual.Email));
+            Assert.AreEqual(expected.FirstName, actual.FirstName, string.Format("Expected FirstName of {0}, actual was {1}", expected.FirstName, actual.FirstName));
+            Assert.AreEqual(expected.LastName, actual.LastName, string.Format("Expected LastName of {0}, actual was {1}", expected.LastName, actual.LastName));
+            Assert.AreEqual(expected.UserId, actual.UserId, string.Format("Expected UserId of {0}, actual was {1}", expected.UserId, actual.UserId));
+        }
+    }
+}
---
+++ UnitTestWorkshop/UnitTestWorkshop.Data.IntegrationTests/Repositories/UserRepository_tests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoDB.Driver;
+using Moq;
+using SMG.Core;
+using UnitTestWorkshop.Data.Models.AccountModels;
+using UnitTestWorkshop.Data.Models.QueryModels;
+using UnitTestWorkshop.Data.Repositories;
+
+namespace UnitTestWorkshop.Data.IntegrationTests.Repositories
+{
+    [TestClass]
+    [Ignore]
+    public class UserRepository_tests
+    {
+        private static UserRepository _subject;
+        private static Mock<ISystemTime> _mockSystemTime;
+        private static string _connectionString;
+        private static MongoServer _server;
+        private static MongoDatabase _database;
+        private List<User> _testData;
+
+        [ClassInitialize]
+        [Description("Initializes only once per full test run.")]
+        public static void InitializeRepo(TestContext context)
+        {
+            _mockSystemTime = new Mock<ISystemTime>();
+            _subject = new UserRepository(_mockSystemTime.Object);
+
+            _connectionString = ConfigurationManager.ConnectionStrings["TestingMongo"].ConnectionString;
+            var client = new MongoClient(_connectionString);
+            _server = client.GetServer();
+            _database = _server.GetDatabase(new MongoUrl(_connectionString).DatabaseName);
+        }
+
+        [TestInitialize]
+        [Description("Initializes before every Test.")]
+        public void InitializeTestData()
+        {
+            _testData = new List<User>
+            {
+                new User
+                {
+                    CreationDate = new DateTime(1999,10,30),
+                    Email = "JimBob@OMGDonuts.com",
+                    FirstName = "Jim",
+                    LastName = "Bob",
+                    LastLogin = new DateTime(2015,12,2)
+                },
+                new User
+                {
+                    CreationDate = new DateTime(1974,04,28),
+                    Email = "JaneBob@OMGCarrots.com",
+                    FirstName = "Jane",
+                    LastName = "Bob",
+                    LastLogin = new DateTime(2015,12,1)
+                }
+            };
+
+            var collection = _database.GetCollection<User>("users");
+            foreach (var user in _testData)
+            {
+                collection.Insert(user);
+            }
+        }
+
+        [TestMethod]
+        public void Should_Retrieve_Single_User_by_Email()
+        {
+            //Arrange
+            var expected = _testData.First();
+            
+            var input = new ByUserEmail
+            {
+                UserEmail = expected.Email
+            };
+
+            User actual;
+
+            //Act
+            actual = _subject.Retrieve(input);
+
+            //Assert
+            Assert.AreEqual(expected.CreationDate, actual.CreationDate, string.Format("Expected:{0} for CreationDate, Actual was {1}", expected.CreationDate, actual.CreationDate));
+            Assert.AreEqual(expected.Email, actual.Email, string.Format("Expected:{0} for Email, Actual was {1}", expected.Email, actual.Email));
+            Assert.AreEqual(expected.FirstName, actual.FirstName, string.Format("Expected:{0} for FirstName, Actual was {1}", expected.FirstName, actual.FirstName));
+            Assert.AreEqual(expected.LastName, actual.LastName, string.Format("Expected:{0} for LastName, Actual was {1}", expected.LastName, actual.LastName));
+            Assert.AreEqual(expected.LastLogin, actual.LastLogin, string.Format("Expected:{0} for LastLogin, Actual was {1}", expected.LastLogin, actual.LastLogin));
+            Assert.AreEqual(expected.UserId, actual.UserId, string.Format("Expected:{0} for UserId, Actual was {1}", expected.UserId, actual.UserId));
+
+        }
+
+        [TestCleanup]
+        [Description("Runs after each test has been run.")]
+        public void CleanupTestData()
+        {
+            _server.DropDatabase(new MongoUrl(_connectionString).DatabaseName);
+        }
+    }
+}
